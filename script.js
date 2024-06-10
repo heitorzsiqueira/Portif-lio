303cfdf813e1734e2a5202e56021e279e893046d
heitor <zeferinoheitor44@gmail.com>
2024-06-10 19:21:46 -0300

    Alterações

---
+++ script.js
@@ -0,0 +1,17 @@
+function abrirMenu() {
+    document.getElementById("Menu_Recolhivel").style.width = "250px";
+  }
+  
+  function fecharMenu() {
+    document.getElementById("Menu_Recolhivel").style.width = "0";
+  }
+
+  function crescer(){
+    document.getElementById("Perfil").style.width = "100%" ;
+  }
+
+  function decrescer(){
+    document.getElementById("Perfil").style.width = "80%" ;
+  }
+
+  
