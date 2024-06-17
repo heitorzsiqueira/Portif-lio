d259eff0be8fff3d0aea61576105bb89aa38d0c3
heitor <zeferinoheitor44@gmail.com>
2024-06-17 16:03:27 -0300

    atualizacao

--- Efects.js
+++ Efects.js
@@ -1,13 +1,5 @@
 
 
-  function crescer(){
-    document.getElementById("Perfil").style.width = "50%" ;
-  }
-
-  function decrescer(){
-    document.getElementById("Perfil").style.width = "40%" ;
-  }
-
   function scroll(){
     document.documentElement.style.scrollBehavior = "smooth";
   }
